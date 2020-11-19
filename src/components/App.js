7ee70e3f2e64e725ab622cef059c5c2632d6816c
sarori <salo3678@gmail.com>
2020-11-19 23:58:31 +0100

    .

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import AppRouter from "./Router";
+import fbase from "fbase";
 
 function App() {
-  return (
-    <div></div>
-  );
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return <AppRouter isLoggedIn={isLoggedIn} />;
 }
 
 export default App;
