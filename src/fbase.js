70cc7ed3bcc6ea8bc34a2ef565094711901c7d70
sarori <salo3678@gmail.com>
2020-11-23 00:29:09 +0100

    Log out

--- src/fbase.js
+++ src/fbase.js
@@ -13,5 +13,6 @@ const firebaseConfig = {
 
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+export const firebaseInstance = firebase;
 
 export const authService = firebase.auth();
