4f9c8956538f221f4f566dae1e6b0e3769cf2797
sarori <salo3678@gmail.com>
2021-02-02 00:14:06 +0100

    .

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,17 +1,29 @@
-import { authService } from 'fbase';
-import React from 'react';
-import {useHistory} from "react-router-dom";
+import { authService, dbService } from "fbase"
+import React, { useEffect } from "react"
+import { useHistory } from "react-router-dom"
 
-const Profile = () => {
-    const history = useHistory();
-    const onLogOutClick = () =>  {
-        authService.signOut();
-        history.push("/");
-    };
-    return (
-        <>
-            <button onClick={onLogOutClick}>Log out</button>
-        </>
-    );
-};
-export default Profile;
+const Profile = ({ userObj }) => {
+	const history = useHistory()
+	const onLogOutClick = () => {
+		authService.signOut()
+		history.push("/")
+	}
+	const getMyNweets = async () => {
+		const nweets = await dbService
+			.collection("nweets")
+			.where("creatorId", "==", userObj.uid)
+			.orderBy("createAt")
+			.get()
+		console.log(nweets.docs.map((doc) => doc.data))
+	}
+	useEffect(() => {
+		getMyNweets()
+	}, [])
+
+	return (
+		<>
+			<button onClick={onLogOutClick}>Log out</button>
+		</>
+	)
+}
+export default Profile
